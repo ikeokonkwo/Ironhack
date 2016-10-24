7c3b9ca1640ae6a04c2ac6b4d9818feda69ebffb
Ike Okonkwo <ikeokonkwo@college.harvard.edu>
2016-10-24 12:19:28 -0400

    ran succesful RSPEC for the 4 features

--- Week_2/Day_1/unit_testing/spec/string_calculator_spec.rb
+++ Week_2/Day_1/unit_testing/spec/string_calculator_spec.rb
@@ -1,15 +1,27 @@
 #Go up one, outside of spec.rb 
 require_relative("../lib/string_calculator.rb")
 
+the_calc = StringCalculator.new
 RSpec.describe StringCalculator do 
-describe "#add" do
-	it "adds 2 numbers separated by commas" do 
-		the_calc = StringCalculator.new 
+	describe "#add" do
+		it "adds 2 numbers separated by commas" do 
+			 
+			expect(the_calc.add("7,7")).to eq(14) 
+			expect(the_calc.add("8,4")).to eq(12) 
+		end 
 
-		expect(the_calc.add("7,7")).to eq(14) 
 
-		expect(the_calc.add("8,4")).to eq(12) 
-	   end
+		it "returns the number for a single number" do 
+			expect(the_calc.add("6")).to eq(6)
+			expect(the_calc.add("3")).to eq(3)
+		end
 
-   end
-end 
+		it "returns 0 for the empty string" do 
+			expect(the_calc.add("")).to eq(0)
+		end 
+
+	    it "returns 0 when no argument given" do
+	   		expect(the_calc.add()).to eq(0)
+	   	end 
+	end 
+end
