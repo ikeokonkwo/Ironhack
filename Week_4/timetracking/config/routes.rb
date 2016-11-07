5fabfad9fbd70b9b9d2853eab48b8c940d04d803
Ike Okonkwo <ikeokonkwo@college.harvard.edu>
2016-11-07 11:10:25 -0500

    first site using rails

--- Week_4/timetracking/config/routes.rb
+++ Week_4/timetracking/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get "/", to: "site#home"
+
+get "/about", to: "site#about"
+get "/contact", to: "site#contact"
 end
