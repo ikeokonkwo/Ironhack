e4ad00c84474ee78c1988d7cb94ca958b918ad49
Ike Okonkwo <ikeokonkwo@college.harvard.edu>
2016-11-08 11:39:25 -0500

    first schema

---
+++ Week_4/day1/timetracking/app/controllers/site_controller.rb
@@ -0,0 +1,12 @@
+class SiteController < ApplicationController
+	def home
+		render 'home' #name of view template
+	end
+	def contact
+		render 'contact' 
+	end 
+	def say_name
+		@name = params[:name]
+		render'say_name'
+	end 
+end
---
+++ Week_4/day1/timetracking/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get "/", to: "site#home"
+
+get "/about", to: "site#about"
+get "/contact", to: "site#contact"
+get'/say_name/:name', to: 'site#say_name'
+
+
+get "/calculator", to: "calculator#add_form"
+post "/calculate", to: "calculator#process_addition"
+
+end
