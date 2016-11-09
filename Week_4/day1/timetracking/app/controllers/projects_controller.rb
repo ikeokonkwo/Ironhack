6007d3d10b11026c1ed385a9f5fa263d42694a00
Ike Okonkwo <ikeokonkwo@college.harvard.edu>
2016-11-09 11:19:20 -0500

    Tmtrckng

--- Week_4/day1/timetracking/app/controllers/projects_controller.rb
+++ Week_4/day1/timetracking/app/controllers/projects_controller.rb
@@ -5,4 +5,20 @@ class ProjectsController < ApplicationController
 		render 'index'
 
 	end
+	def show
+		@my_project = Project.find(params[:id])
+		render 'show'
+	end 
+	def new
+		@my_project = Project.new
+		render "new"
+	end 
+	def create
+		@my_project = Project.new(
+			:name => params[:project][:name],
+			:description => params[:project][:description])
+		@my_project.save
+
+		redirect_to project_path(@my_project)
+	end 
 end 
