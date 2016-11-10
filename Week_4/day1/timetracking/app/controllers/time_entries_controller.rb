ad7f20427542d72ef8c5478a4c8b3770f8ecd327
Ike Okonkwo <ikeokonkwo@college.harvard.edu>
2016-11-10 14:01:03 -0500

    routing in Tmtrck

--- Week_4/day1/timetracking/app/controllers/time_entries_controller.rb
+++ Week_4/day1/timetracking/app/controllers/time_entries_controller.rb
@@ -14,12 +14,34 @@ class TimeEntriesController < ApplicationController
 			comments: params[:time_entry][:comments],
 			minutes: params[:time_entry][:minutes],
 			date: params[:time_entry][:date])
-		
+
 		if @my_entry.save
 			redirect_to project_time_entries_path(@my_project)
 		else 
 			render "new"
 		end 
+	end
+	def edit 
+		@my_project = Project.find params[:project_id]
+		@my_entry = @my_project.time_entries.find params[:id]
 
 	end 
+
+	def update
+		@my_project = Project.find_by(id: params[:project_id])
+		@my_entry = @my_project.time_entries.find_by(id: params[:id])
+
+		if @my_entry.update(entry_params)
+
+		redirect_to project_time_entries_path(@my_project)
+		else 
+			render :edit
+		end 
+	end 
+
+	private
+	def  entry_params
+		params.require(:time_entry).permit(:hours, :minutes, :date, :comments)
+	end 
 end
+ 
--- Week_4/day1/timetracking/config/routes.rb
+++ Week_4/day1/timetracking/config/routes.rb
@@ -28,5 +28,11 @@ get '/projects/:project_id/time_entries/new', to: 'time_entries#new', as: "new_p
 
 post "/projects/:project_id/time_entries", to: "time_entries#create", as: "project_time_entries"
 
+get "/projects/:project_id/time_entries/:id/edit", to: "time_entries#edit", as: "edit_project_time_entry"
+patch "/projects/:project_id/time_entries/:id", to: "time_entries#update", as: "project_time_entry"
+
+
+
+
 
 end
