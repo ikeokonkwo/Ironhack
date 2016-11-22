dfbc5300de1e1b33a1dbf81ba8027c3a572d7e6c
Ike Okonkwo <ikeokonkwo@college.harvard.edu>
2016-11-22 14:20:35 -0500

    working user registration

---
+++ Week_6/auth-exercise-master/app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+	def new
+		if session[:user_id]
+			redirect_to "/"
+		else
+			render :new
+		end
+	end  
+
+	def create 
+		user_maybe =  User.find_by(email: params[:email])
+
+		if user_maybe ==nil || user_maybe.authenticate(params[:password]) == false
+			redirect_to "/login"
+		else 
+			session[:user_id] = user_maybe.id 
+			redirect_to "/"
+	end
+	end  
+
+	def destroy
+		# session[:user_id] = nil
+		session.clear
+		redirect_to "/"
+	end 
+end
+
