5ee4592e0413c54b1055a5d8b415c934ffceda74
Ike Okonkwo <ikeokonkwo@college.harvard.edu>
2018-02-06 14:08:44 -0500

    create and rake Client model

--- CMD_App/config/routes.rb
+++ CMD_App/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
 get '/', to: 'main#chat'
+
+root to: "main#chat"
 
 end
--- CMD_App/db/schema.rb
+++ CMD_App/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115215156) do
+ActiveRecord::Schema.define(version: 20180206190635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_clients_on_user_id", using: :btree
+  end
 
   create_table "user_models", force: :cascade do |t|
     t.string   "email"
@@ -36,11 +43,11 @@ ActiveRecord::Schema.define(version: 20180115215156) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "clients", "users"
 end
