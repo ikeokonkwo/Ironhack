67d03a675f4b0fd18b99091f4a28b30fdf307ec5
Ike Okonkwo <ikeokonkwo@college.harvard.edu>
2018-02-06 19:23:35 -0500

    Devise Working

---
+++ CMD_App/db/migrate/20180206191739_drop_user_models.rb
@@ -0,0 +1,5 @@
+class DropUserModels < ActiveRecord::Migration[5.0]
+  def change
+  	# drop_table :user_model
+  end
+end
--- CMD_App/db/schema.rb
+++ CMD_App/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206190635) do
+ActiveRecord::Schema.define(version: 20180206191739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,16 +20,6 @@ ActiveRecord::Schema.define(version: 20180206190635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_clients_on_user_id", using: :btree
-  end
-
-  create_table "user_models", force: :cascade do |t|
-    t.string   "email"
-    t.string   "name"
-    t.string   "specialty"
-    t.boolean  "is_subscribed"
-    t.integer  "phone"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
